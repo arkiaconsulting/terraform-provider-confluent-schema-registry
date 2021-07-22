f019e6942d278388b7a9fc13230bbc36c93311e7
Noureddine Seddik <noureddine.seddik@olx.com>
2021-07-22 21:05:13 +0100

    Examples with schema references support

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     schemaregistry = {
-      version = "0.4"
+      version = "0.6"
       source  = "github.com/arkiaconsulting/schemaregistry"
     }
   }
@@ -16,8 +16,19 @@ resource "schemaregistry_schema" "user_added" {
   schema  = file("./userAdded.avsc")
 }
 
+resource "schemaregistry_schema" "with_reference" {
+  subject = "with-reference"
+  schema = "[\"akc.test.userAdded\"]"
+
+  references {
+    name = "akc.test.userAdded"
+    subject = schemaregistry_schema.user_added.subject
+    version = schemaregistry_schema.user_added.version
+  }
+}
+
 data "schemaregistry_schema" "main" {
-  subject = schemaregistry_schema.user_added.subject
+  subject = schemaregistry_schema.with_reference.subject
 }
 
 output "schema_id" {
@@ -34,4 +45,8 @@ output "schema_string" {
 
 output "schema_schema_id" {
   value = data.schemaregistry_schema.main.schema_id
+}
+
+output "schema_references" {
+  value = data.schemaregistry_schema.main.references
 }
