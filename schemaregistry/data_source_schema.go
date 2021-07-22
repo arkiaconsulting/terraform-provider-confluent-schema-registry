70599601b2c08434a5bd985a8036e24e5d1802c0
Noureddine Seddik <noureddine.seddik@olx.com>
2021-07-22 20:43:57 +0100

    DataSource with schema references support

--- schemaregistry/data_source_schema.go
+++ schemaregistry/data_source_schema.go
@@ -32,6 +32,30 @@ func dataSourceSchema() *schema.Resource {
 				Computed:    true,
 				Description: "The schema string",
 			},
+			"references": {
+				Type:        schema.TypeList,
+				Computed: 	 true,
+				Description: "The referenced schema names list",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The referenced schema name",
+						},
+						"subject": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The subject related to the schema",
+						},
+						"version": {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "The version of the schema",
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -53,7 +77,28 @@ func dataSourceSubjectRead(ctx context.Context, d *schema.ResourceData, m interf
 	d.Set("schema_id", schema.ID())
 	d.Set("version", schema.Version())
 
+	if err = d.Set("references", FromRegistryReferences(schema.References())); err != nil {
+		return diag.FromErr(err)
+	}
+
 	d.SetId(formatSchemaVersionID(subject))
 
 	return diags
+}
+
+func FromRegistryReferences(references []srclient.Reference) []interface{} {
+	if len(references) == 0 {
+		return make([]interface{}, 0)
+	}
+
+	refs := make([]interface{}, 0, len(references))
+	for _, reference := range references {
+		refs = append(refs, map[string]interface{}{
+			"name": reference.Name,
+			"subject": reference.Subject,
+			"version": reference.Version,
+		})
+	}
+
+	return refs
 }
--- schemaregistry/data_source_schema_test.go
+++ schemaregistry/data_source_schema_test.go
@@ -2,6 +2,9 @@ package schemaregistry
 
 import (
 	"fmt"
+	"github.com/riferrei/srclient"
+	"os"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -28,6 +31,77 @@ func TestAccDataSourceSchema_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("data.schemaregistry_schema.test", "version", "1"),
 					resource.TestCheckResourceAttr("data.schemaregistry_schema.test", "schema", strings.Replace(fixtureAvro1, "\\", "", -1)),
 					resource.TestCheckResourceAttrSet("data.schemaregistry_schema.test", "schema_id"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceSchemaReferences_basic(t *testing.T) {
+	// GIVEN
+	url, found := os.LookupEnv("SCHEMA_REGISTRY_URL")
+	if !found {
+		t.Fatalf("SCHEMA_REGISTRY_URL must be set for acceptance tests")
+	}
+	username := os.Getenv("SCHEMA_REGISTRY_USERNAME")
+	password := os.Getenv("SCHEMA_REGISTRY_PASSWORD")
+
+	client := srclient.CreateSchemaRegistryClient(url)
+	if (username != "") && (password != "") {
+		client.SetCredentials(username, password)
+	}
+
+	// AND
+	u, err := uuid.GenerateUUID()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	referencedSchemaSubject := fmt.Sprintf("referencedSub-%s", u)
+	referencedSchema := strings.Replace(fixtureAvro1, "\\", "", -1)
+
+	schemaWithReferenceSubject := fmt.Sprintf("sub-%s", u)
+	schemaWithReference := `["akc.test.userAdded"]`
+
+	references := []srclient.Reference{
+		{
+			Name:    "akc.test.userAdded",
+			Subject: referencedSchemaSubject,
+			Version: 1,
+		},
+	}
+
+	// AND
+	if _, err = client.CreateSchemaWithArbitrarySubject(referencedSchemaSubject, referencedSchema, srclient.Avro); err != nil {
+		t.Fatalf("could not create schema for subject: %s, err: %s", referencedSchema, err)
+	}
+
+	if _, err = client.CreateSchemaWithArbitrarySubject(schemaWithReferenceSubject, schemaWithReference, srclient.Avro, references...); err != nil {
+		t.Fatalf("could not create schema for subject: %s, err: %s", referencedSchemaSubject, err)
+	}
+
+	// WHEN / THEN
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviders,
+		PreCheck:          func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					data "schemaregistry_schema" "schemaWithReference" {
+						subject = "%s"
+					}
+				`, schemaWithReferenceSubject),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "id", schemaWithReferenceSubject),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "subject", schemaWithReferenceSubject),
+					resource.TestCheckResourceAttrSet("data.schemaregistry_schema.schemaWithReference", "schema_id"),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "version", "1"),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "schema", schemaWithReference),
+
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "references.#", "1"),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "references.0.name", references[0].Name),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "references.0.subject", references[0].Subject),
+					resource.TestCheckResourceAttr("data.schemaregistry_schema.schemaWithReference", "references.0.version", strconv.Itoa(references[0].Version)),
 				),
 			},
 		},
